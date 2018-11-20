675cea1e171a5dc895e3ceb09b7ba1e91332d4a7
Eelis Takala <eelis.takala@gmail.com>
2018-11-20 14:00:15 +0100

    Refactor more modular

--- readroxie.py
+++ readroxie.py
@@ -1,37 +1,53 @@
+def parse_version(roxiedata, content):
+    section = ''
+    for i,line in enumerate(content):
+        splitline = line.replace('\n','').split()
+        if 'VERSION' in line: 
+            roxiedata['VERSION'] = splitline[1]
+            return
+
+def parse_cadata(roxiedata, content):
+    section = ''
+    for i,line in enumerate(content):
+        splitline = line.replace('\n','').split()
+        if '.cadata' in line: 
+            roxiedata['cadata'] = splitline[0].replace("'",'')
+            return
+
+def parse_section(roxiedata, content, key, stopkey):
+    section = ''
+    for i,line in enumerate(content):
+        splitline = line.replace('\n','').split()
+
+        if not len(splitline) == 0:
+            if key == splitline[0]: 
+                section = key
+                roxiedata[key] = []
+                section_lines = []
+
+        if section == key : 
+            if stopkey in line:
+                header_vars = splitline
+                data = []
+                for section_line in section_lines:
+                    data.append({})
+                    for i, header_var in enumerate(header_vars):
+                        data[-1][header_var] = section_line[i]
+                section = ''
+                roxiedata[key] = data
+            elif 'BLOCK' not in line:
+                section_lines.append(splitline)
+
+
+
 
 filename = 'TEST.data'
 with open(filename) as f:
 	content = f.readlines()
 
 roxiedata = {}
-section = ''
-for i,line in enumerate(content):
-    splitline = line.replace('\n','').split()
-    if 'VERSION' in line: roxiedata['VERSION'] = splitline[1]
+parse_version(roxiedata, content)
+parse_cadata(roxiedata, content)
+parse_section(roxiedata, content, 'BLOCK', 'alpha')
 
-    section_keys = ['BLOCK']
-    for key in section_keys:
-        if not len(splitline) == 0:
-            if key == splitline[0]: 
-                section = key
-                roxiedata[key] = []
-                section_lines = []
-                break
-
-    if section == 'BLOCK' : 
-        if 'alpha' in line:
-            header_vars = splitline
-            data = []
-            for section_line in section_lines:
-                data.append({})
-                for i, header_var in enumerate(header_vars):
-                    data[-1][header_var] = section_line[i]
-            section = ''
-            roxiedata['BLOCK'] = data
-        elif 'BLOCK' not in line:
-            section_lines.append(splitline)
-
-
-print roxiedata['BLOCK'][3]['radius']
-
-
+print roxiedata
